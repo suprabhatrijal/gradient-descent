c57dab23e77dcdf9c9a793f833976ea103ace272
Suprabhat Rijal <goodguysman@gmail.com>
2020-06-06 19:28:05 +0545

    Comments
    Added comments

--- gradient-descent.py
+++ gradient-descent.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 
+#li is the list of points to do linear regression on
 li= [(8,3),(18,10),(11,4),(13,6),(14,8),(22,12),(12,5),(9,4),(20,9),(25,14)]
+#m a random slope, it way be any value
 m = 1
+# c is a random intercept, it maybe any value
 c = 500
+#learning_rate is the learning rate
 learning_rate = 0.0001
 
 
-
+#simple function that takes data(points) to be predicted, slope and intercept of the model to give the predicted value
 def predict(points, slope, intercept):
     predict_list = []
     for point in points:
@@ -15,7 +19,7 @@ def predict(points, slope, intercept):
         predict_list.append(y_predict)
     return predict_list
 
-
+#Gives the RSS of the points and the prediction
 def RSS(points, prediction):
     rss = 0
     for i in range(0, len(prediction)):
@@ -26,7 +30,7 @@ def RSS(points, prediction):
         rss += (y-y_predict)**2
     return rss
 
-
+#finds the derivative of the RSS w.r.t. slope and intercept
 def RSS_derivative(points,slope, intercept):
     d_slope = 0
     d_intercept = 0
@@ -38,9 +42,13 @@ def RSS_derivative(points,slope, intercept):
 
     return  {'slope':d_slope, 'intercept':d_intercept}
 
+#dictionary of derivatives of RSS w.r.t. slope and intercept 
 derivatives = RSS_derivative(li,m,c)
-step_count = 0
+
+
 print("RSS before regression: ", RSS(li, predict(li,m,c)))
+
+#algorithm
 while abs(derivatives['slope']) >= 0.001 or abs(derivatives['intercept']) >= 0.001:
     step_slope = derivatives['slope'] * learning_rate
     step_intercept = derivatives['intercept'] * learning_rate
@@ -50,9 +58,12 @@ while abs(derivatives['slope']) >= 0.001 or abs(derivatives['intercept']) >= 0.0
     derivatives = RSS_derivative(li, m, c)
 
 
-    step_count += 1
-print("Slope: ",m, "Intercept: ",c)
 
+print("Slope: ",m, "Intercept: ",c)
+print("RSS after regression: ", RSS(li, predict(li,m,c)))
+
+
+#Matplotlib to display the graph
 xs = [x[0] for x in li]
 ys = [x[1] for x in li]
 
@@ -71,4 +82,3 @@ for i_x, i_y in zip(xs, ys):
 
 plt.show()
 
-print("RSS after regression: ", RSS(li, predict(li,m,c)))
